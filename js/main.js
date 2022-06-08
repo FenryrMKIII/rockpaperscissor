3c8dc002a84255277624d921a41dfea1fe573bcc
jbulle <jbulle@jbulle>
2022-06-08 21:19:54 +0200

    feat: implemented game()

--- js/main.js
+++ js/main.js
@@ -23,28 +23,51 @@ function playRound(playerSelection, computerSelection) {
     const playerSelectionSerialized = playerSelection.toLowerCase();
 
     if (playerSelection == computerSelection) {
-        return "Draw!"
+        return ["Draw!", "draw"]
     } else {
         for (array of losing_combinatons_player) {
             player_move = array[0]
             computer_move = array[1]
             if ((player_move == playerSelection) && (computer_move == computerSelection)) {
-                return "You Lose! " + computerSelection + " beats " + playerSelection
+                return ["You Lose! " + computerSelection + " beats " + playerSelection, "computer"]
             }
         }
 
-        return "You Won! " + playerSelection + " beats " + computerSelection
+        return ["You Won! " + playerSelection + " beats " + computerSelection, "player"]
     }
 
 
-
-    for (const color of colors)
-
-
-        return "You Lose! Paper beats Rock"
+    return "You Lose! Paper beats Rock"
 }
 
-console.log("let's play rock paper scissors!")
-console.log("Player chose " + playerSelection)
-console.log("Computer chose " + computerSelection)
-console.log(playRound(playerSelection, computerSelection))
+function game() {
+    console.log("let's play rock paper scissors!")
+
+    let computer_score = 0;
+    let player_score = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let round = i + 1
+        console.log("Round number " + round.toString())
+
+        console.log("Player chose " + playerSelection)
+        console.log("Computer chose " + computerSelection)
+        result = playRound(playerSelection, computerSelection)
+        console.log(result[0])
+
+        if (result[1] == "computer") {
+            computer_score++
+        } else {
+            player_score++
+        }
+
+        if (player_score > computer_score) {
+            console.log("You won!")
+        } else {
+            console.log("You lost!")
+        }
+
+    }
+}
+
+game()
